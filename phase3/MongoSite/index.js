4a5743be248939e9bfa4411ae71b0638c3e956ba
2058590 Josue L Madrid <2058590@tcs.com>
2021-09-09 00:32:59 -0700

    add html files and express code to index.js

---
+++ phase3/MongoSite/index.js
@@ -0,0 +1,71 @@
+let fs = require("fs");
+let express = require("express");
+let bodyParser = require("body-parser");
+const { json } = require("body-parser");
+
+let app = express();
+
+let mongoClient = require("mongodb").MongoClient;
+ 
+let url ="mongodb://localhost:27017";
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/index", (request, response) => {
+   
+    response.sendFile(__dirname+"\\index.html");
+});
+
+app.post("/action_add", (request, response) => {
+    taskData = request.body;
+    console.log(taskData);
+    //addTask(taskData);
+    //writeTasks();
+
+    response.sendFile(__dirname+"\\add.html");    
+});
+
+app.post("/action_update", (request, response) => {
+    taskData = request.body;
+    console.log(taskData);
+    response.sendFile(__dirname+"\\update.html");    
+});
+
+app.post("/action_delete", (request, response) => {
+    taskData = request.body;
+    console.log(taskData);
+    response.sendFile(__dirname+"\\delete.html");    
+});
+
+app.post("/action_fetch", (request, response) => {
+    taskData = request.body;
+    console.log(taskData);
+    response.sendFile(__dirname+"\\fetch.html");    
+});
+
+function addDocument(dbname, collection, data)
+{
+    mongoClient.connect(url, (err,client) => {
+        if(!err){
+            console.log("Connected")
+            let db = client.db(dbname);
+           
+           
+                db.collection(collection).insertMany(data, 
+                (err,result)=> {
+                    if(!err){
+                      console.log("Record inserted successfully")
+                       console.log(result);
+                    }else {
+                        console.log(err);
+                    }
+                    client.close();
+                });
+        }
+    });
+}
+
+app.listen(9090,()=>console.log("Server running on port number 9090"));
